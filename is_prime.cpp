9eb87e2a6e558b069408d386573a0edbf88dd0e0
appleaww <artemmorozz2006@gmail.com>
2025-01-04 01:17:18 +0300

    Create is_prime.cpp

---
+++ is_prime.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int erat(int n){
+    int numbers[n+1];
+    for(int i = 0; i<=n;i++){
+        numbers[i] = true;
+    }
+    numbers[0] = numbers[1]= false;
+    for(int i = 2; i*i<=n; i++){
+        if(numbers[i] == true){
+            for(int j = i*i; j<=n; j+=i){
+                numbers[j] = false;
+            }
+        }
+    }
+    for (int p = 2; p <= n; p++){
+        if (numbers[p]){
+            cout << p << " ";
+        }
+}
+}
+int main(){
+    int n;
+    cin >> n;
+    erat(n);
+    return 0;
+}
+
